2791abdde65dcf3ec74c316ef198206fb7743683
Samyukta1909 <sahoosamyukta@gmail.com>
2022-10-08 10:49:24 +0530

    First Commit

---
+++ app/src/main/java/com/example/dummy/ModelClass.java
@@ -0,0 +1,12 @@
+package com.example.dummy;
+
+public class ModelClass {
+    public String exp;
+    public  int amount;
+
+    public ModelClass(String exp, int amount) {
+        this.exp = exp;
+        this.amount = amount;
+    }
+
+}
