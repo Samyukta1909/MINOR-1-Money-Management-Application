f65e15b6e32785f5aef3855b8d331df89eace524
Samyukta1909 <sahoosamyukta@gmail.com>
2022-10-19 12:00:26 +0530

    updates

--- app/src/main/java/com/example/dummy/HomeActivity.java
+++ app/src/main/java/com/example/dummy/HomeActivity.java
@@ -2,13 +2,39 @@ package com.example.dummy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button button2,button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent1 = new Intent(HomeActivity.this,ExpenseTrackerActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent2 = new Intent(HomeActivity.this,SplitwiseActivity.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
