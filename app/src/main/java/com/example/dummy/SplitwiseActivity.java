f95281f4df9027050dedba9b2f5d2ca0e597853a
Karma Nakba <nerdynerd0905@gmail.com>
2022-12-17 14:46:13 +0530

    updates

--- app/src/main/java/com/example/dummy/SplitwiseActivity.java
+++ app/src/main/java/com/example/dummy/SplitwiseActivity.java
@@ -2,13 +2,49 @@ package com.example.dummy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class SplitwiseActivity extends AppCompatActivity {
+
+    private ListView detlistview;
+    static List printBill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splitwise);
+
+        initWidgets();
+        setdetAdapter();
+    }
+
+    private void setdetAdapter() {
+
+        DetailsAdapter detailsAdapter = new DetailsAdapter(getApplicationContext(), Detail.detailsArrayList);
+        detlistview.setAdapter(detailsAdapter);
+    }
+
+    private void initWidgets() {
+        detlistview = findViewById(R.id.group_listview);
+    }
+
+
+    public void newDet(View view) {
+
+        Intent newDetIntent = new Intent(this, DetailActivity.class);
+        startActivity(newDetIntent);
+    }
+
+
+
+    public void FINDPATH(View view) {
+
+        Intent splitIntent = new Intent(this, SplitwiseCal.class);
+        startActivity(splitIntent);
     }
 }
